a3c5b4594f2c6befc9477a6f32a0139b39d255bd
Annika Klüpfel <AnnikaKluepfel@web.de>
2020-12-08 15:09:11 +0100

    included basic map w/o fetching data using test array. Info window content ist not rendered.

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 
 import AllEntries from './Entries/AllEntries';
+import GoogleMap from './Map/GoogleMap';
 import EntryDetail from './Entries/EntryDetail';
 import Contact from './Contact/Contact';
 
@@ -16,16 +17,22 @@ function Body() {
       <Router>
         <div className="body-wrap">
           
-          {/* ENTRIES SECTION */}
+          {/* ALL ENTRIES + MAP */}
             <Route 
             exact path='/' 
             render={() => (
+              <>
                 <div className='all-entries-wrap'>
-                <AllEntries entries={entries}/>
+                  <AllEntries entries={entries}/>
                 </div>
+                <div className='map-wrap'>
+                  <GoogleMap/>
+                </div>
+              </>
               )}
             />
-      
+
+            {/* ENTRY DETAIL PAGE */}
             <Route 
               exact path={`/post/:id`}
               render={() => (
@@ -36,16 +43,6 @@ function Body() {
                 </>
               )}
             />
-          
-          {/* MAP SECTION */}
-          <Route 
-            exact path={`/`}
-            component={() => (
-              <div className='map-wrap'>
-                <div>map test</div>
-              </div>
-            )}
-          />
                   
         {/*as it always confuses me, reminder: below is "body-wrap" div closing tag...*/}
         </div>
---
+++ src/Body/Map/GoogleMap.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+import './Marker.css';
+import Theme from './Theme';
+
+import Geocode from 'react-geocode';
+Geocode.setApiKey(process.env.API_KEY);
+
+let test = [
+    {
+        "location" : "Hamburg",
+        lat: 53.551085,
+        lng: 9.993682,
+        "description": "Wonderful hanseatic city"
+    },
+    {
+        "location" : "Lübeck",
+        lat : 53.86546735,
+        lng : 10.6865593,
+        "description": "Ich liebe Marzipan."
+    },
+    {
+      "location" : "Hannover",
+      lat : 52.3758916,
+      lng : 9.7320104,
+      "description": "Let's see..."
+  }
+]
+
+const Marker = props => (
+
+    <React.Fragment>
+      <div
+        className='pin bounce'
+      />
+      {/* if show is set to true > additional div as iinfo window */}
+      {props.show && (
+        <div className='infoWindow'>
+          <div 
+            onClick={props.exit}
+            className='closeWindow'
+          >
+            x
+          </div> <br/>
+          <h3>{props.title}</h3> <br/>
+          {props.desc}
+        </div>
+      )}
+    </React.Fragment>
+)
+ 
+class GoogleMap extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        center: {
+          lat: 53.551085,
+          lng: 9.993682
+        },
+        zoom: 12,
+        show: false
+      }
+    }
+    
+    _onChildClick = (key) => {
+      this.setState({show: true});
+      console.log(key);
+    }
+
+    closeWindow = () => this.setState({show: false})
+  
+    render() {
+      return (
+
+        <div style={{height: "100vh", width: "100%"}}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key: process.env.API_KEY}}
+                defaultCenter={this.state.center}
+                center={this.props.currentCenter}
+                defaultZoom={this.state.zoom}
+                onChildClick={this._onChildClick}
+                options={{styles: Theme}}
+            >
+
+                {test.map(place => (
+                    <Marker
+                        key={place.location}
+                        title={place.location}
+                        desc={place.description}
+                        lat={place.lat}
+                        lng={place.lng}
+                        show={this.state.show}
+                        exit={this.closeWindow}
+                    />
+                ))}
+
+            </GoogleMapReact>
+        </div>
+      )
+    }
+  }
+
+  export default GoogleMap;
