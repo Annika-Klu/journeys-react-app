008bc11abc156cc20fd9bb4cc12bd5863ee35b67
Annika Klüpfel <AnnikaKluepfel@web.de>
2021-01-26 17:58:08 +0100

    added post request & updated input attributes for email submission form

--- src/Body/Contact/Contact.js
+++ src/Body/Contact/Contact.js
@@ -1,4 +1,24 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import  axios from 'axios';
+
 function Contact() {
+
+    const [message, setMessage] = useState({});
+    const history = useHistory();
+
+    async function submitMessage (event) {
+        event.preventDefault();
+        console.log(message);
+        axios.post('/contact', message)
+             .then(function (response) {
+             console.log(response); })
+             .catch(function (error) {
+             console.log(error);}
+         );
+         history.push('/');
+    }
+
     return (
         <div className='single-page box'>
             <div className='single-page-content'>
@@ -24,23 +44,48 @@ function Contact() {
                     <br/> ...I sure don't mind going "back to school" if it's with you!
                 </p>
                 <div>
-                    <p class='title sub-title'>Wanna get in touch?</p>
+                    <p class='title sub-title'>Wanna see code? Visit me on github</p>
                     <p>github.com/annika-klu</p>
+                    <p class='title sub-title'>Wanna get in touch?</p>
                     <p class='red'>Note: at present, this contact form is 
                         <span class='bold'> not </span>
                     functional. The server will shortly be reprogrammed to enable sending a messsage through this contact form.</p>
-                    <form>
-                        <label for='yourEmail'>Your email 
+                    <form onSubmit={submitMessage}>
+                        <label htmlFor='yourName'>Your name 
                             <br/>
-                            <input id='yourEmail' type='text'></input>
+                            <input 
+                                name='yourName' 
+                                type='text'
+                                value={message.yourName}
+                                onChange={event => setMessage({...message, [event.currentTarget.name] : event.currentTarget.value})}
+                                required>
+                                
+                            </input>
                         </label>
                         <br/>
-                        <label for='yourMessage'>Your message
+                        <label htmlFor='yourEmail'>Your email 
                             <br/>
-                            <input id='yourMessage' type='text'></input>
+                            <input
+                                name='yourEmail'
+                                type='email'
+                                value={message.yourEmail}
+                                onChange={event => setMessage({...message, [event.currentTarget.name] : event.currentTarget.value})}
+                                required>
+                            </input>
                         </label>
                         <br/>
-                        <button id='sendMessage'>Send</button>
+                        <label htmlFor='yourMessage'>Your message
+                            <br/>
+                            <input
+                                name='yourMessage' 
+                                type='text'
+                                value={message.text}
+                                onChange={event => setMessage({...message, [event.currentTarget.name] : event.currentTarget.value})}
+                                required>
+                            </input>
+                        </label>
+                        <br/>
+                        <button id='sendMessage' type='submit'>Send</button>
                     </form>
                 </div>
             </div>
